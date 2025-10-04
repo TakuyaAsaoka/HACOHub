3595636e45f7881a83f67038de13c9386101b4c1
Asaoka Takuya <dimas080201@gmail.com>
2025-10-05 02:45:20 +0900

    HacoHubTabViewを作った

---
+++ HACOHub/Views/Components/HacoHubTabView.swift
@@ -0,0 +1,18 @@
+//
+//  HacoHubTabView.swift
+//  HACOHub
+//
+//  Created by AsaokaTakuya on 2025/10/04.
+//
+
+import SwiftUI
+
+struct HacoHubTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HacoHubTabView()
+}
