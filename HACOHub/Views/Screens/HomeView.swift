12eac71214ffa89db3be60c87d75674080e0a593
Asaoka Takuya <dimas080201@gmail.com>
2025-10-06 09:36:54 +0900

    HomeViewのいらないところを消した

--- HACOHub/Views/Screens/HomeView.swift
+++ HACOHub/Views/Screens/HomeView.swift
@@ -8,9 +8,6 @@
 import SwiftUI
 
 struct HomeView: View {
-  @State private var situation: Situation? = nil
-  @State private var showMapView: Bool = false
-
   struct SituationButton: Identifiable, Hashable {
     let id = UUID()
     let text: String
@@ -26,19 +23,11 @@ struct HomeView: View {
     SituationButton(text: "Medical", situation: .medical)
   ]
 
-  let columns = getColumns(n: 2, spacing: 12)
-
   var body: some View {
     ZStack(alignment: .top) {
-      VStack(spacing: 40) {
         Text.sfProBold("HACOHub", size: 64)
-          .foregroundColor(.white)
-          .padding(.top, 120)
-        }
-        .background(Color.white.opacity(0.56))
-        .cornerRadius(16)
-        .shadow(color: Color.black.opacity(0.2), radius: 32, x: 0, y: 0)
-      }
+          .foregroundColor(.black)
+    }
   }
 }
 
