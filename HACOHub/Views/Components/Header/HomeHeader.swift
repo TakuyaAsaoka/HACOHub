559574f7665621263ce4c2ac24eef92b1be8afe5
Asaoka Takuya <dimas080201@gmail.com>
2025-10-10 21:29:20 +0900

    HomeHeaderを作った

---
+++ HACOHub/Views/Components/Header/HomeHeader.swift
@@ -0,0 +1,37 @@
+//
+//  HomeHeader.swift
+//  HACOHub
+//
+//  Created by AsaokaTakuya on 2025/10/10.
+//
+
+import SwiftUI
+
+struct HomeHeader: View {
+  let title: String
+
+  var body: some View {
+    VStack {
+      Spacer()
+      HStack {
+        Text.sfProBold(title, size: 20)
+        Spacer()
+      }
+      .padding(.bottom, 15)
+    }
+    .padding(.leading, 20)
+    .frame(height: 103)
+    .background(Color.white)
+  }
+}
+
+#Preview {
+    ZStack {
+      Color.gray
+      VStack {
+        HomeHeader(title: "My Locker")
+        Spacer()
+    }
+  }
+  .ignoresSafeArea()
+}
