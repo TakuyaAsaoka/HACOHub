e756b13e88fd1507fc45f88f424d17bfa21bf676
badorikiriki-sys <badorikiriki@gmail.com>
2025-10-12 17:04:24 +0900

    HomeView
    楕円状にアイコンを配置

--- HACOHub/Views/Screens/HomeView.swift
+++ HACOHub/Views/Screens/HomeView.swift
@@ -29,197 +29,81 @@ struct HomeView: View {
 
   var body: some View {
     GeometryReader { geomerty in
-      VStack(spacing: 0) {
+      VStack(spacing: 10) {
         HomeHeaderView()
+          
+        //小さい円情報
+        let Buttons:[(String, String, Color)] = [
+          ("Delivery", "DeliveryIcon", getRGBColor(255, 248, 222)),
+          ("Event", "EventIcon", getRGBColor(255, 235, 220)),
+          ("Send", "SendIcon", getRGBColor(210, 247, 222)),
+          ("Education", "EducationIcon", getRGBColor(205, 246, 250)),
+          ("Lent", "LentIcon", getRGBColor(243, 231, 255)),
+          ("Medical", "MedicalIcon", getRGBColor(222, 237, 255))
+          //("Books", "BooksIcon", getRGBColor(255, 235, 235)),
+         
+        ]
+          
+        //配置する円の半径(小さい円の中心が置かれる線路円の半径)
+        let middleRadius = ((bigCirclesize / 2) - (smallCirclesize / 2)) - 10
+          let ellipseRadiusX = (bigCirclesize / 2) - (smallCirclesize / 2) * 2.2 // 横方向の短い半径
+          let ellipseRadiusY = (bigCirclesize / 2) - (smallCirclesize / 2) * 1.2 // 縦方向の長い半径
+          
         //中段サークル
-        ZStack(alignment: .top){
+        ZStack{
           //大きな円
           Circle()
             .foregroundColor(getRGBColor(236, 249, 243))
-//            .frame(width: bigCirclesize, height: bigCirclesize)
-            .clipped()
-          //
-          Group{
-            //                      //小さな円１
-            //                      Button {
-            //                          // TODO: 通知一覧を表示する
-            //                          print()
-            //                      } label: {
-            //                          ZStack{
-            //                              Circle()
-            //                                  .foregroundColor(getRGBColor(255, 235, 220))
-            //                                  .frame(width: smallCirclesize, height: smallCirclesize)
-            //
-            //                              VStack(spacing:0){
-            //                                  Image("EventIcon")
-            //                                      .resizable()
-            //                                      .frame(width: 64, height: 64)
-            //                                  Text("Event")
-            //                                      .font(.system(size: 24, weight: .bold))
-            //                                      .foregroundColor(.black)
-            //                                      .multilineTextAlignment(.center).font(.caption)
-            //                              }
-            //                          }
-            //                      }
-            //                          .position(x:154 + smallCircleRadius, y: 27 + smallCircleRadius)
-            //
-            //
-            //                      //小さな円2
-            //                      Button {
-            //                          // TODO: 通知一覧を表示する
-            //                          print()
-            //                      } label: {
-            //                          ZStack{
-            //                              Circle()
-            //                                  .foregroundColor(getRGBColor(255, 248, 222))
-            //                                  .frame(width: smallCirclesize, height: smallCirclesize)
-            //
-            //                              VStack(spacing:0){
-            //                                  Image("DeliveryIcon")
-            //                                      .resizable()
-            //                                      .frame(width: 64, height: 64)
-            //                                  Text("Delivery")
-            //                                      .font(.system(size: 24, weight: .bold))
-            //                                      .foregroundColor(.black)
-            //                                      .multilineTextAlignment(.center).font(.caption)
-            //                              }
-            //                          }
-            //                      }
-            //                          .position(x:353 + smallCircleRadius, y:30 + smallCircleRadius)
-            //
-            //                      //小さな円3
-            //                      Button {
-            //                          // TODO: 通知一覧を表示する
-            //                          print()
-            //                      } label: {
-            //                          ZStack{
-            //                              Circle()
-            //                                  .foregroundColor(getRGBColor(222, 237, 255))
-            //                                  .frame(width: smallCirclesize, height: smallCirclesize)
-            //
-            //                              VStack(spacing:0){
-            //                                  Image("MedicalIcon")
-            //                                      .resizable()
-            //                                      .frame(width: 64, height: 64)
-            //                                  Text("Medical")
-            //                                      .font(.system(size: 24, weight: .bold))
-            //                                      .foregroundColor(.black)
-            //                                      .multilineTextAlignment(.center).font(.caption)
-            //                              }
-            //                          }
-            //                      }
-            //                          .position(x:118 + smallCircleRadius, y:193 + smallCircleRadius)
-            //
-            //                      //小さな円4
-            //                      Button {
-            //                          // TODO: 通知一覧を表示する
-            //                          print()
-            //                      } label: {
-            //                          ZStack{
-            //                              Circle()
-            //                                  .foregroundColor(getRGBColor(210, 247, 222))
-            //                                  .frame(width: smallCirclesize, height: smallCirclesize)
-            //
-            //                              VStack(spacing:0){
-            //                                  Image("SendIcon")
-            //                                      .resizable()
-            //                                      .frame(width: 64, height: 64)
-            //                                  Text("Send")
-            //                                      .font(.system(size: 24, weight: .bold))
-            //                                      .foregroundColor(.black)
-            //                                      .multilineTextAlignment(.center).font(.caption)
-            //                              }
-            //                          }
-            //                      }
-            //                          .position(x:271 + smallCircleRadius, y:160 + smallCircleRadius)
-            //
-            //                      //小さな円5
-            //                      Button {
-            //                          // TODO: 通知一覧を表示する
-            //                          print()
-            //                      } label: {
-            //                          ZStack{
-            //                              Circle()
-            //                                  .foregroundColor(getRGBColor(243, 231, 255))
-            //                                  .frame(width: smallCirclesize, height: smallCirclesize)
-            //
-            //                              VStack(spacing:0){
-            //                                  Image("LentIcon")
-            //                                      .resizable()
-            //                                      .frame(width: 64, height: 64)
-            //                                  Text("Lent")
-            //                                      .font(.system(size: 24, weight: .bold))
-            //                                      .foregroundColor(.black)
-            //                                      .multilineTextAlignment(.center).font(.caption)
-            //                              }
-            //                          }
-            //                      }
-            //                          .position(x:168 + smallCircleRadius, y:345 + smallCircleRadius)
-            //
-            //                      //小さな円6
-            //                      Button {
-            //                          // TODO: 通知一覧を表示する
-            //                          print()
-            //                      } label: {
-            //                          ZStack{
-            //                              Circle()
-            //                                  .foregroundColor(getRGBColor(255, 235, 235))
-            //                                  .frame(width: smallCirclesize, height: smallCirclesize)
-            //
-            //                              VStack(spacing:0){
-            //                                  Image("BooksIcon")
-            //                                      .resizable()
-            //                                      .frame(width: 64, height: 64)
-            //                                  Text("Books")
-            //                                      .font(.system(size: 24, weight: .bold))
-            //                                      .foregroundColor(.black)
-            //                                      .multilineTextAlignment(.center).font(.caption)
-            //                              }
-            //                          }
-            //                      }
-            //                          .position(x:344 + smallCircleRadius, y:303 + smallCircleRadius)
-
-            //小さな円7
-            Button {
-              // TODO: 通知一覧を表示する
-              print()
-            } label: {
-              ZStack{
-                Circle()
-                  .foregroundColor(getRGBColor(205, 246, 250))
-                  .frame(width: smallCirclesize, height: smallCirclesize)
-
-                VStack(spacing:0){
-                  Image("EducationIcon")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-                  Text("Education")
-                    .font(.system(size: 24, weight: .bold))
-                    .foregroundColor(.black)
-                    .multilineTextAlignment(.center).font(.caption)
+            .frame(width: bigCirclesize, height: bigCirclesize)
+          //中央アイコン
+          Image("TrackIcon")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 179, height: 179)
+                .offset(y: -bigCirclesize / 2 + bigCirclesize / 2)
+            
+          
+          
+          //小さい円を７つ放射状に配置
+            ForEach(0..<Buttons.count, id: \.self) { i in
+                let angle = Angle(degrees: Double(i) * (360.0 / Double(Buttons.count)) - 90)
+                
+                let label = Buttons[i].0
+                let icon = Buttons[i].1
+                let color = Buttons[i].2
+                
+                Button{
+                } label: {
+                    ZStack{
+                        Circle()
+                            .foregroundColor(color)
+                            .frame(width: smallCirclesize, height: smallCirclesize)
+                        
+                        VStack(spacing:0){
+                            Image(icon)
+                                .resizable()
+                                .frame(width: 64, height: 64)
+                            Text(label)
+                                .font(.system(size: 24, weight: .bold))
+                                .foregroundColor(.black)
+                                .multilineTextAlignment(.center).font(.caption)
+                        }
+                    }
                 }
-              }
+                
+                .offset(
+//                    x: CGFloat(cos(angle.radians - .pi / 2)) * middleRadius,
+//                    y: CGFloat(sin(angle.radians - .pi / 2)) * middleRadius
+                    x: CGFloat(cos(angle.radians)) * ellipseRadiusX,
+                    y: CGFloat(sin(angle.radians)) * ellipseRadiusY
+                )
+                .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 4)
             }
-            //                          .position(x:338 + smallCircleRadius, y:457 + smallCircleRadius)
-
-          }
-          .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 4)
-
-          //HacoHubIcon挿入
-          let imagewidth: CGFloat = 195
-          let imageheight: CGFloat = 195
-          //              Image("HacoHubIcon")
-          //                  .resizable()
-          //                  .scaledToFit()
-          //                  .frame(width: imagewidth, height: imageheight)
-          //                  .border(.blue)
-          //                  .position(x:118 + imagewidth / 2, y:471 + imageheight / 2)
         }
-        //              .frame(width: .infinity, height: .infinity)
-        .border(.red)
+//        .border(.red)
       }
       .ignoresSafeArea()
-      .border(.blue)
+//      .border(.blue)
       .frame(width: geomerty.size.width, height: .infinity)
     }
   }
