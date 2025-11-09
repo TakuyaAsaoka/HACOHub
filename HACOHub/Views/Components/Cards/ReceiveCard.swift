694ea0b79db7a0996f4015abe95bfdd359001a91
Asaoka Takuya <dimas080201@gmail.com>
2025-11-09 09:16:40 +0900

    ReceiveCardを修正した

--- HACOHub/Views/Components/Cards/ReceiveCard.swift
+++ HACOHub/Views/Components/Cards/ReceiveCard.swift
@@ -15,57 +15,62 @@ struct ReceiveCard: View {
 		Button {
 			path.append(Route.changePickUpLocation)
 		} label: {
-			VStack(alignment: .leading, spacing: 8) {
-				HStack(alignment: .top, spacing: 12) {
-					Image("iPhone")
-						.resizable()
-						.scaledToFit()
-						.frame(width: 48, height: 48)
-					
-					VStack(alignment: .leading, spacing: 10) {
-						Text.sfProRegular("From Amazon", size: 12)
-							.foregroundColor(getRGBColor(106, 114, 130))
-						
-						Text.sfProRegular("iPhone 20", size: 14)
-							.foregroundColor(getRGBColor(30, 41, 57))
-						
-						HStack(spacing: 4) {
-							Image("GreenLugageIcon")
-								.resizable()
-								.scaledToFit()
-								.frame(width: 14, height: 14)
-							Text.sfProRegular("Small", size: 12)
-								.foregroundColor(getRGBColor(74, 85, 101))
-						}
-						
-						HStack(spacing: 4) {
-							Image("GreenTrackIcon")
-								.resizable()
-								.scaledToFit()
-								.frame(width: 14, height: 14)
-							Text.sfProRegular("7th St NE, Atlanta, GA 30308", size: 12)
-								.foregroundColor(getRGBColor(74, 85, 101))
-						}
-						
-						Text.sfProRegular("Shipping No: EC119214", size: 12)
-							.foregroundColor(getRGBColor(54, 65, 83))
+			VStack(alignment: .leading, spacing: 16) {
+        ZStack(alignment: .topLeading) {
+          HStack(alignment: .top, spacing: 12) {
+            Image("iPhone")
+              .resizable()
+              .scaledToFit()
+              .frame(width: 48, height: 48)
 
-					}
-					.padding(.top, 4)
-					
-					Spacer()
-					
-					Text.sfProMedium("Receive", size: 14)
-						.foregroundColor(getRGBColor(230, 20, 146))
-						.padding(.vertical, 4)       // 上下の余白
-						.padding(.horizontal, 17)    // 左右の余白
-						.background(getRGBColor(254, 219, 233))
-						.overlay(
-								RoundedRectangle(cornerRadius: 12)
-										.stroke(getRGBColor(255, 190, 215), lineWidth: 1) // 水色の枠線
-						)
-						.cornerRadius(12)
-				}
+            VStack(alignment: .leading, spacing: 10) {
+              Text.sfProRegular("From Amazon", size: 12)
+                .foregroundColor(getRGBColor(106, 114, 130))
+
+              Text.sfProRegular("iPhone 20", size: 14)
+                .foregroundColor(getRGBColor(30, 41, 57))
+
+              HStack(spacing: 4) {
+                Image("GreenLugageIcon")
+                  .resizable()
+                  .scaledToFit()
+                  .frame(width: 14, height: 14)
+                Text.sfProRegular("Small", size: 12)
+                  .foregroundColor(getRGBColor(74, 85, 101))
+              }
+
+              HStack(spacing: 4) {
+                Image("GreenTrackIcon")
+                  .resizable()
+                  .scaledToFit()
+                  .frame(width: 14, height: 14)
+                Text.sfProRegular("7th St NE, Atlanta, GA 30308", size: 12)
+                  .foregroundColor(getRGBColor(74, 85, 101))
+              }
+
+              Divider()
+                .background(getRGBColor(243, 244, 246))
+
+              Text.sfProRegular("Shipping No: EC119214", size: 12)
+                .foregroundColor(getRGBColor(106, 114, 130))
+            }
+            .padding(.top, 4)
+          }
+
+          HStack(alignment: .top, spacing: 0) {
+            Spacer()
+            Text.sfProMedium("Receive", size: 14)
+              .foregroundColor(getRGBColor(230, 20, 146))
+              .padding(.vertical, 4)
+              .padding(.horizontal, 17)
+              .background(getRGBColor(254, 219, 233))
+              .overlay(
+                RoundedRectangle(cornerRadius: 12)
+                  .stroke(getRGBColor(255, 190, 215), lineWidth: 1)
+              )
+              .cornerRadius(12)
+          }
+        }
 
 				PrimaryRoundedButton(
 					text: "Scan a QR code",
@@ -77,7 +82,8 @@ struct ReceiveCard: View {
 						isShowingQR = true
 					}
 				)
-				
+        .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 2)
+
 				WhiteRoundedButton(
 					text: "Change pickup location",
 					textColor: getRGBColor(36, 41, 47),
@@ -89,7 +95,7 @@ struct ReceiveCard: View {
 						path.append(Route.changePickUpLocation)
 					}
 				)
-				.shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 2)
+				.shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 2)
 			}
 			.padding(.top, 17)
 			.padding(.bottom, 19)
